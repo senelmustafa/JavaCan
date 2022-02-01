87fea1e22e250d8d774edb13155b71906353c6b3
Mustafa ŞENEL <87585970+senelmustafa@users.noreply.github.com>
2022-02-01 21:15:52 +0300

    Add files via upload

---
+++ WhileLoop1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WhileLoop1 {
+
+	public static void main(String[] args) {
+// Soru 6 ) Kullanicidan bir sayi alin 
+//          ve bu sayiyi tam bolen sayilari 
+//          ve toplam kac tane olduklarini ekranda yazdirin
+		
+		
+		for(int num=1; num<2; num++){
+
+			   System.out.print(num + " ");
+
+			}
+		}
+	}
+ 
+			
+ 
